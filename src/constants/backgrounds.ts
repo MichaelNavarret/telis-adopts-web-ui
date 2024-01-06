db46363b25316d3fdeda8f84930d14fa4001ae1f
Michael Navarrete <michael.07navarrete@gmail.coim>
2024-01-06 13:34:23 -0300

    Fix Backgrounds

--- src/constants/backgrounds.ts
+++ src/constants/backgrounds.ts
@@ -1,19 +1,19 @@
 import main_background from "../assets/backgrounds/bg_main.png";
-import lannies_background from "../assets/backgrounds/bg_lannies.png";
+import lanies_background from "../assets/backgrounds/bg_lanies.png";
 import spectralumen_background from "../assets/backgrounds/bg_spectralumen.png";
 import cloudystars_background from "../assets/backgrounds/bg_cloudystars.png";
 import plunies_background from "../assets/backgrounds/bg_plunies.png";
 import { getCurrentSpecie } from "../tools/commons";
 
 export const MAIN_BACKGROUND = main_background;
-export const LANNIES_BACKGROUND = lannies_background;
+export const LANIES_BACKGROUND = lanies_background;
 export const SPECTRALUMEN_BACKGROUND = spectralumen_background;
 export const CLOUDYSTARS_BACKGROUND = cloudystars_background;
 export const PLUNIES_BACKGROUND = plunies_background;
 
 export const speciesBackgrounds = {
   main: MAIN_BACKGROUND,
-  lannies: LANNIES_BACKGROUND,
+  lanies: LANIES_BACKGROUND,
   spectralumen: SPECTRALUMEN_BACKGROUND,
   cloudystars: CLOUDYSTARS_BACKGROUND,
   plunies: PLUNIES_BACKGROUND,
@@ -22,8 +22,8 @@ export const speciesBackgrounds = {
 export function getBackground() {
   const species = getCurrentSpecie();
   switch (species) {
-    case "lannies":
-      return LANNIES_BACKGROUND;
+    case "lanies":
+      return LANIES_BACKGROUND;
     case "spectralumen":
       return SPECTRALUMEN_BACKGROUND;
     case "cloudystars":
